c4734bc37fdd2fa15943cfdc4e3c771e7d0139fd
Eryl <eryl.murphy@gmail.com>
2018-07-14 18:31:54 -0400

    trying to get computed style

--- src/ShowAlbum/ShowAlbum.js
+++ src/ShowAlbum/ShowAlbum.js
@@ -1,22 +1,38 @@
 import React, { Component } from "react";
 import TopAlbums from '../TopAlbums/TopAlbums';
-
+import Palette from "react-palette";
 
 class ShowAlbum extends Component {
   constructor(props) {
     super(props);
     this.state = {
       matchAlbums: [],
-      albumImage : ''
+      albumImage : null,
     }
   }
   
   render() {
-
+    function getTheStyle(){
+    const coloredHead = document.getElementsByClassName('.coloredhead');
+    const style = window.getComputedStyle(coloredHead, null).getPropertyValue("color");
+    document.getElementsByClassName('.output').innerHTML = style;
+    }
     console.log(this.props)
     return (
       <div className="show-album">
-        <img src={this.props.albumImage} style={{border : '20px red solid'}}/>
+        <div>
+          <Palette image={this.props.albumImage}>
+            {palette => (
+              <div style={{ color: palette.vibrant }}>
+                <h1 className="coloredhead">Vibrant Color</h1>
+                <div className="output">Hello</div>
+                <img src={this.props.albumImage}/>
+                {/* {console.log(style)} */}
+              </div>
+            )}
+          </Palette>
+
+        </div>
       </div>
     )  
 }
--- src/TopAlbums/TopAlbums.js
+++ src/TopAlbums/TopAlbums.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import './TopAlbums.css';
 // import GetColor from '../GetColor/GetColor';
-import PasteImage from '../PasteImage/PasteImage';
+
 import Palette from "react-palette";
 import ShowAlbum from '../ShowAlbum/ShowAlbum';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
@@ -16,7 +16,6 @@ class TopAlbums extends Component {
       albumImage: '',
       showComponent: false,
       isHidden: true,
-      // isSelected: false
     }
     this.search = this.search.bind(this);
     this.handleClick = this.handleClick.bind(this);
@@ -27,13 +26,13 @@ class TopAlbums extends Component {
     fetch(baseURL, {
       method: 'GET'
     }).then(response => response.json())
-    .then(json => {
-      this.setState({
-        index: index,
-        albumImage: albumImage,
-      });
-      console.log(this.state.albumImage);
-    })
+      .then(json => {
+        this.setState({
+          index: index,
+          albumImage: albumImage,
+        });
+        console.log(this.state.albumImage);
+      })
   };
 
   toggleHidden() {
@@ -42,7 +41,8 @@ class TopAlbums extends Component {
     })
   }
 
-search() {
+
+  search() {
     const baseURL = `https://ws.audioscrobbler.com/2.0/?method=artist.gettopalbums&artist=${this.state.query}&api_key=750edfe3fb22652664ad82d8e4e9cd8b&format=json`;
     fetch(baseURL, {
       method: 'GET'
@@ -64,32 +64,26 @@ search() {
           <input type="text" className="form" placeholder="Enter Any Artist to See Albums" required onChange={event => this.setState({ query: event.target.value })} onKeyPress={event => { if (event.key === 'Enter') this.search() }} />
           <div className="topalbums">
             <div>
-              <Palette image={this.state.albumImage}>
-                {palette => (
-                  <div style={{ color: palette.vibrant }}>
+  <div>
                     <h1>See all albums for {this.state.query}</h1>
-                  </div> 
-                )}
-              </Palette>
+                  </div>
             </div>
+            <div>{TopAlbums && <ShowAlbum albumImage={this.state.albumImage} />}</div>
             <ul onClick={this.toggleHidden.bind(this)}>
               {
                 this.state.albums.map(function (album, index) {
-                  
+
                   const albumImage = album.image[2]['#text'];
                   return (
                     <li className='albums' key={index}>
-                    <Router>
-                      <Link to='/ShowAlbum'> 
+
                       <img className="img" src={album.image[2]['#text']} onClick={(evt) => this.handleClick(evt, index, albumImage)} />
-                      <Route path="/ShowAlbum" exact component={ShowAlbum} />
-                      </Link>
-                      </Router>
-            <div>  
-              <ShowAlbum albumImage={this.albumImage} />
-              </div>
-                    </li> )}.bind(this))}
-            </ul>{!this.state.isHidden && <PasteImage />}
+
+                      <div>
+                      </div>
+                    </li>)
+                }.bind(this))}
+            </ul>{!this.state.isHidden}
           </div>
         </div>
       </div>
