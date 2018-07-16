8863d61accb1d843ca776f21478a0633c649ee58
Eryl <eryl.murphy@gmail.com>
2018-07-16 15:24:21 -0400

    removed db;

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 require('dotenv').config()
 const express = require('express');
 const app = express();
-const CompAlbum = require('./models/Compalbum');
+
 const bodyParser = require("body-parser");
-require('../../TopAlbums/TopAlbums.js');
 
 app.use(bodyParser.urlencoded({
   extended: true
@@ -15,28 +14,27 @@ const session = require("express-session");
 
 const PORT = process.env.PORT || 3001;
 
-console.log(process.env.API_KEY);
 
-app.get("/", (request, response) => {
-  response.send(`hello world`);
-});
+// app.get("/", (request, response) => {
+//   response.send(`hello world`);
+// });
 
 
-app.get('/compalbums.json', (request, response) => {
-  CompAlbum.all()
-  .then(compalbums => {
-    console.log(compalbums)
-    response.json(compalbums)
-  });
-});
+// app.get('/compalbums.json', (request, response) => {
+//   CompAlbum.all()
+//   .then(compalbums => {
+//     console.log(compalbums)
+//     response.json(compalbums)
+//   });
+// });
 
-app.get('/compalbums/color_name.json', (request, response) => {
-  Compalbum.allOfColor(request.params.color_name)
-  .then(compalbums => {
-    console.log(compalbums)
-    response.json(compalbums)
-  });
-});
+// app.get('/compalbums/color_name.json', (request, response) => {
+//   Compalbum.allOfColor(request.params.color_name)
+//   .then(compalbums => {
+//     console.log(compalbums)
+//     response.json(compalbums)
+//   });
+// });
 
 app.listen(PORT, () => {
 console.log(`Express web server listening on port ${PORT}`);
